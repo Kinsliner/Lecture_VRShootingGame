43e33575ab97bca279167d5b07a452a993d36ab3
Kinsliner <kinsliner@gmail.com>
2020-06-09 02:03:00 +0800

    [更新]敵人物件

---
+++ Assets/Scripts/AttackEventReciver.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackEventReciver : MonoBehaviour {
+
+	public void AttackEvent()
+	{
+		SendMessageUpwards("SendDamage");
+	}
+}
