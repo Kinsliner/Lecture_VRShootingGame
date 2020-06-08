d2e1b54ba51bcc9c00d7906db6bfe7af5493dcb0
Kinsliner <kinsliner@gmail.com>
2020-06-08 21:23:51 +0800

    [更新]SteamVR功能設定

---
+++ Assets/Scripts/ViveInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveInput : MonoBehaviour {
+
+	private Gun gun;
+
+	// Use this for initialization
+	void Start () {
+
+		gun = FindObjectOfType<Gun>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		bool state = SteamVR_Actions.default_GrabPinch.GetStateDown(SteamVR_Input_Sources.Any);
+		gun.SetFireState(state);
+	}
+}
